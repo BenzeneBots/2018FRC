c19d3a81e7e724559735ede955f3da1515e5a48b
y2kmurali <y2kmurali@gmail.com>
2018-01-25 18:26:57 -0500

    Created intake files

---
+++ workspace/BB2/src/Subsystems/Intake.cpp
@@ -0,0 +1,14 @@
+/*
+ * Intake.cpp
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: Murali
+ */
+
+#include <Subsystems/Intake.h>
+
+Intake::Intake() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ workspace/BB2/src/Subsystems/Intake.h
@@ -0,0 +1,16 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Jan 25, 2018
+ *      Author: Murali
+ */
+
+#ifndef SRC_SUBSYSTEMS_INTAKE_H_
+#define SRC_SUBSYSTEMS_INTAKE_H_
+
+class Intake {
+public:
+	Intake(int);
+};
+
+#endif /* SRC_SUBSYSTEMS_INTAKE_H_ */
