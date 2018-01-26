4280547b36b2d3c0bd3c3c7c78965dcab481103b
y2kmurali <y2kmurali@gmail.com>
2018-01-25 23:18:44 -0500

    Created and began working on intake and elevator systems

--- workspace/BB2/src/Subsystems/Intake.h
+++ workspace/BB2/src/Subsystems/Intake.h
@@ -8,9 +8,22 @@
 #ifndef SRC_SUBSYSTEMS_INTAKE_H_
 #define SRC_SUBSYSTEMS_INTAKE_H_
 
+#include <WPILib.h>
+
+namespace Intake {
+
 class Intake {
 public:
-	Intake(int);
+	Intake(int, int, int);
+	void IntakeCubes();
+	void OuttakeCubes();
+	void StopIntake();
+	void TiltIntake();
+
+private:
+	Victor *intake1, *intake2, *angleMotor;
 };
+
+} /* namespace Intake */
 
 #endif /* SRC_SUBSYSTEMS_INTAKE_H_ */
