0469b2551076df2ea2986278a6aa46e51dfbb8f8
y2kmurali <y2kmurali@gmail.com>
2018-01-28 15:49:53 -0500

    Updated Drive files

---
+++ workspace/BB2/src/Subsystems/Drive.cpp
@@ -0,0 +1,17 @@
+/*
+ * Drive.cpp
+ *
+ *  Created on: Jan 28, 2018
+ *      Author: Murali
+ */
+
+#include <Subsystems/Drive.h>
+
+namespace Drive {
+
+Drive::Drive() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace Drive */
---
+++ workspace/BB2/src/Subsystems/Drive.h
@@ -0,0 +1,20 @@
+/*
+ * Drive.h
+ *
+ *  Created on: Jan 28, 2018
+ *      Author: Murali
+ */
+
+#ifndef SRC_SUBSYSTEMS_DRIVE_H_
+#define SRC_SUBSYSTEMS_DRIVE_H_
+
+namespace Drive {
+
+class Drive {
+public:
+	Drive();
+};
+
+} /* namespace Drive */
+
+#endif /* SRC_SUBSYSTEMS_DRIVE_H_ */
