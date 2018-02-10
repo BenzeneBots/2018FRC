b0ba596942c6edb73bea652dd19eb5739fa58228
rinaik <rinaik@hotmail.com>
2018-02-10 12:50:48 -0500

    minor cosmetic fixes

--- workspace/BB2/src/Robot.cpp
+++ workspace/BB2/src/Robot.cpp
@@ -24,11 +24,12 @@
 
 class Robot : public frc::TimedRobot {
 public:
-	PigeonIMU * _pidgey;
+
 	//Initialize all the subystems
 	Intake::Intake *robotIntake;
 	Drive::Drive *robotDrive;
 	Elevator::Elevator *robotElevator;
+	PigeonIMU *_pidgey;
 
 	//Init joysticks
 	Joystick *mainDriverStick, *secondaryDriverStick, *manipStick;
@@ -41,7 +42,7 @@ public:
 		//initialize subsystems
 		robotDrive = new Drive::Drive(0,1,4,5); 			//drive uses Talons 0,1,2,3
 		robotElevator = new Elevator::Elevator(4,5,4); 		//elevator uses Talon 4 and DIOs 4 and 5
-				_pidgey = new PigeonIMU(0);
+		_pidgey = new PigeonIMU(0);
 
 
 	}
