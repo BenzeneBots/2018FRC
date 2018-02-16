6564d967dad6d436dfe38512fa420698b1d07ad7
y2kmurali <y2kmurali@gmail.com>
2018-02-16 15:21:14 -0500

    Created auto command

---
+++ workspace/BB2/src/Auton/AutoCommand.cpp
@@ -0,0 +1,14 @@
+/*
+ * AutoCommand.cpp
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: Murali
+ */
+
+#include <Auton/AutoCommand.h>
+
+AutoCommand::AutoCommand() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ workspace/BB2/src/Auton/AutoCommand.h
@@ -0,0 +1,16 @@
+/*
+ * AutoCommand.h
+ *
+ *  Created on: Feb 13, 2018
+ *      Author: Murali
+ */
+
+#ifndef SRC_AUTON_AUTOCOMMAND_H_
+#define SRC_AUTON_AUTOCOMMAND_H_
+
+class AutoCommand {
+public:
+	AutoCommand();
+};
+
+#endif /* SRC_AUTON_AUTOCOMMAND_H_ */
